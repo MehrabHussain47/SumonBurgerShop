4fcdff9c68babb4e7b1da261882032a3c0708c67
Asus <mehhussumon47@gmail.com>
2025-02-02 18:50:31 +0600

    Again Final Commit

--- app/src/main/java/com/example/sumonburgershop/MainActivity.java
+++ app/src/main/java/com/example/sumonburgershop/MainActivity.java
@@ -35,37 +35,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-//                String email = editTextEmail.getText().toString();
-//                String password = editTextPassword.getText().toString();
-//
-//                editTextEmail.setError(null);
-//                editTextPassword.setError(null);
-//
-//                String emailPattern = "^[a-zA-Z0-9._%+-]+@gmail\\.com$";
-//                String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
-//
-//                if (email.isEmpty() || password.isEmpty()) {
-//                    Toast.makeText(getApplicationContext(), "Fields are empty", Toast.LENGTH_SHORT).show();
-//                } else if (!email.matches(emailPattern)) {
-//                    editTextEmail.setError("Enter a valid Gmail address");
-//                } else if (!password.matches(passwordPattern)) {
-//                    editTextPassword.setError("Password must be at least 6 characters, include letters and numbers");
-//                } else {
-//                    if (email.equals("admin@gmail.com") && password.equals("admin")) {
-//                        Intent intent = new Intent(MainActivity.this, AdminActivity.class);
-//                        startActivity(intent);
-//                    } else {
-//                        boolean checkEmailPassword = db.checkEmailPassword(email, password);
-//                        if (checkEmailPassword) {
-//                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-//                            Intent intent = new Intent(MainActivity.this, ViewActivity.class);
-//                            startActivity(intent);
-//                        } else {
-//                            Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                }
-
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
 
