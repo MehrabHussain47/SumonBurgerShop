b732e64ce25ca49266a160fc4cb1255bbe678177
Asus <mehhussumon47@gmail.com>
2025-03-13 23:45:14 +0600

    No commit

--- app/src/main/java/com/example/sumonburgershop/DeleteActivity.java
+++ app/src/main/java/com/example/sumonburgershop/DeleteActivity.java
@@ -1,6 +1,5 @@
 package com.example.sumonburgershop;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
